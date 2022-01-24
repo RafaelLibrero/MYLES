4735d5218764a564e371426b988e26502ec7e581
Enrique <fpemlktz@gmail.com>
2022-01-24 14:03:48 +0100

    codificando splash

--- app/src/main/java/com/enrashe/myles/Login.java
+++ app/src/main/java/com/enrashe/myles/Login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -12,10 +13,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btnsig = findViewById(R.id.bsignup);
+
     }
 
-    public void lanzar_main(View view){
-        Intent intent=new Intent(this,MainActivity.class);
+
+    public void lanzar_main(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
