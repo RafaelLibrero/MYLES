ef0aea0bb3b248ea113f59873b9bf4fa872c607e
RafaelLibrero <librerorafael@outlook.com>
2022-01-24 12:47:54 +0100

    Botones funcionales

--- app/src/main/java/com/enrashe/myles/Login.java
+++ app/src/main/java/com/enrashe/myles/Login.java
@@ -5,17 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button blogin,bsignup1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    public void lanzar_main(View view){
-        Intent intent=new Intent(this,MainActivity.class);
-        startActivity(intent);
+        blogin = findViewById(R.id.bsignup);
+        bsignup1 = findViewById(R.id.bsignup1);
+
+        blogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bsignup1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Signup.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
