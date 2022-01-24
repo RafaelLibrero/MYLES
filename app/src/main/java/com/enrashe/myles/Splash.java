c09ade1e81da2d98b07bc70159c5b8a2640ac6f3
Enrique <fpemlktz@gmail.com>
2022-01-24 13:51:05 +0100

    transición login-splash

--- app/src/main/java/com/enrashe/myles/Splash.java
+++ app/src/main/java/com/enrashe/myles/Splash.java
@@ -1,9 +1,11 @@
 package com.enrashe.myles;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +16,21 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        openApp(true);
 
         TextView fan2 = (TextView) findViewById(R.id.tv_anim);
-        Animation myanim2= AnimationUtils.loadAnimation(this, R.anim.splash_animation2);
+        Animation myanim2 = AnimationUtils.loadAnimation(this, R.anim.splash_animation2);
         fan2.startAnimation(myanim2);
 
+    }
+    private void openApp(boolean locationPermission){
+        //Handler: hace q1ue se retrase una accion
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent intent = new Intent(Splash.this, Login.class);
+                startActivity(intent);
+            }
+        }, 3100);
     }
 }
