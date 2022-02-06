410a0cba949038a76ca3de8ffbf541af6325f7fc
Rafael <librerorafael@outlook.com>
2022-02-06 03:21:49 +0100

    Botones fragments
    Ahora los botones te llevan al main como hacían antes, y register ha sido cambiado por signup

--- app/src/main/java/com/enrashe/myles/LoginTabFragment.java
+++ app/src/main/java/com/enrashe/myles/LoginTabFragment.java
@@ -1,17 +1,31 @@
 package com.enrashe.myles;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 public class LoginTabFragment extends Fragment {
 
+    private Button blogin;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment, container, false);
 
+        blogin = root.findViewById(R.id.blogin);
+
+        blogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
         return root;
+
     }
 }
--- app/src/main/java/com/enrashe/myles/SignupTabFragment.java
+++ app/src/main/java/com/enrashe/myles/SignupTabFragment.java
@@ -1,17 +1,30 @@
 package com.enrashe.myles;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 public class SignupTabFragment extends Fragment {
 
+    private Button bsignup;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.signup_tab_fragment,container,false);
 
+        bsignup = root.findViewById(R.id.bsignup);
+
+        bsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
         return root;
     }
 }
