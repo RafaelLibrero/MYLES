0e37e7e76bb100bae109e7f09a5bef4d12eb6709
Rafael <librerorafael@outlook.com>
2022-02-08 16:34:25 +0100

    Migracion a viewpager2 para los fragments del login.

--- app/src/main/java/com/enrashe/myles/Login.java
+++ app/src/main/java/com/enrashe/myles/Login.java
@@ -1,16 +1,21 @@
 package com.enrashe.myles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentController;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class Login extends AppCompatActivity {
 
     TabLayout tabLayout;
-    ViewPager viewPager;
+    ViewPager2 viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +29,20 @@ public class Login extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("Signup"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        final LoginAdapter adapter =
-                new LoginAdapter(getSupportFragmentManager(),this,tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
+        viewPager.setAdapter(new LoginAdapter(this));
 
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText(R.string.login);
+                    break;
+                case 1:
+                    tab.setText(R.string.signup);
+                    break;
+            }
+        }).attach();
+
     }
+
+
 }
