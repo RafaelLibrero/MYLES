88f0daf68e7eaf2c615a694b6827177b88b79242
Rafael <librerorafael@outlook.com>
2022-02-25 21:59:45 +0100

    Login y register con firebaseauth

--- app/src/main/java/com/enrashe/myles/LoginTabFragment.java
+++ app/src/main/java/com/enrashe/myles/LoginTabFragment.java
@@ -6,26 +6,63 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginTabFragment extends Fragment {
 
     private Button blogin;
+    private EditText email,password;
+    private FirebaseAuth mAuth;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment, container, false);
 
         blogin = root.findViewById(R.id.blogin);
+        email = root.findViewById(R.id.editTextTextEmail);
+        password = root.findViewById(R.id.editTextTextPassword);
+        mAuth = FirebaseAuth.getInstance();
 
         blogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), MainNavigation.class);
-                startActivity(intent);
+                login();
             }
         });
         return root;
 
+    }
+
+    private void login(){
+        String mail = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if (mail.isEmpty()){
+            email.setError("Email can not be empty");
+        }
+        if (pass.isEmpty()){
+            password.setError("Password can not be empty");
+        }
+        else{
+            mAuth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(getActivity(),"Login Successful",Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getActivity(), MainNavigation.class));
+                    }
+                    else{
+                        Toast.makeText(getActivity(),"Login Failed",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
